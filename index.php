971f21aefe7f3bfe45e9c658bbee3ccae9fc4af8
Ellen Liu <huailun@hotmail.com>
2016-04-13 12:29:21 -0600

    add.gitignore

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
-	<head></head>
+	<head>
 		<meta charset="UTF-8"/>
 		<title>About me</title>
+	</head>
+	<body>
+		<h1>Bach gave us God's word.Mozart gave us God's laughter.Beethoven gave us God's fire.God gave us Music that we might pray without words. </h1>
+		<p>I will sing the Lord’s praise,for he has been good to me</p>
+	</body>
 </html>
