63f786ded3aa58a959e934a04b688d753fe44b6c
Ellen Liu <huailun@hotmail.com>
2016-04-13 14:53:38 -0600

    add.gitignore

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
 		<h1>Bach gave us God's word.Mozart gave us God's laughter.Beethoven gave us God's fire.God gave us Music that we might pray without words. </h1>
 		<header><img src="images/images.png"/></header>
 		<p>How to have fun with piano</p>
-		<footer><img src="images/imagesHAEPXJPK.jpg"></footer>
+		<footer><img src="images/imagesHAEPXJPK.jpg"/></footer>
+		<h2>Must Knows</h2>
+		<menu>
+			<li>How to number your fingers</li>
+			<li>What to read between lines & spaces</li>
+		</menu>
+		
 	</body>
 </html>
